b599f569269f29535feae7433aee7f2eda39a931
dotonebit <83027257+dotonebit@users.noreply.github.com>
2021-05-04 21:09:03 +0300

    Update Problem1.py
    Update variable names and comments to make the code clearer.

--- Problem1.py
+++ Problem1.py
@@ -8,14 +8,14 @@ See: projecteuler.net/problem=1
 
 # Variable to hold the sum.
 # Start at 0.
-result = 0
+sum = 0
 
 # Create a for loop to iterate through numbers up to 1000.
 # Write a condition to test if the number is a multiple of 3 or 5.
-# If the condition is true, add the number to $result.
+# If the condition is true, add the number to the sum.
 for n in range(1000):
     if n % 3 == 0 or n % 5 == 0:
-        result += n
+        sum += n
 
-# Print $result.
-print(result)
+# Print the sum.
+print(sum)
